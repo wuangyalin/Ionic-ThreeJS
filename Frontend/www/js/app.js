8a65b6e5aff583834cac226314f205203794d366
wuangyalin <wuangyalin@gmail.com>
2016-03-16 17:27:39 +1100

    Updated ThreeJS Demo
    Updated ThreeJS Demo

--- Frontend/www/js/app.js
+++ Frontend/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','backand','starter.controllers','starter.services','starter.threeJS','ngCordova'])
+angular.module('starter', ['ionic','backand','starter.controllers','starter.services','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -95,7 +95,8 @@ angular.module('starter', ['ionic','backand','starter.controllers','starter.serv
       url: "/3DRoom",
       views: {
         'menuContent': {
-          templateUrl: "templates/3DRoom.html"
+          templateUrl: "templates/3DRoom.html",
+          controller: 'threeCtrl'
         }
       }
     })
